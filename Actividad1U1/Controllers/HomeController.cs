f3e6d82c415b65fab131d44f7a708346bfcdecf0
171G0213@rcarbonifera.tecnm.mx <171G0213@rcarbonifera.tecnm.mx>
2021-09-22 15:26:16 -0500

    A1

---
+++ Actividad1U1/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Actividad1U1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Programación Web, Semestre 9, periodo Agosto-Diciembre 2021, Adán Daniel Aguilar Romero";
+        }
+
+        public IActionResult Miperfil()
+        {
+            return View();
+        }
+    }
+}
