0e0ea113fe462889053a2be1e9dd6e2b51e27512
171G0213@rcarbonifera.tecnm.mx <171G0213@rcarbonifera.tecnm.mx>
2021-09-22 20:44:12 -0500

    Promedio

---
+++ Actividad2U1/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Actividad2U1.Models;
+
+namespace Actividad2U1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "PROMEDIO CALIFICACIONES";
+        }
+
+    
+
+        public IActionResult PaginaPromedio(int Calificacion1, int Calificacion2, int Calificacion3)
+        {
+            PaginaPromedioViewModel vm = new PaginaPromedioViewModel();
+            vm.Calificacion1 = Calificacion1;
+            vm.Calificacion2 = Calificacion2;
+            vm.Calificacion3 = Calificacion3;
+            return View (vm);
+        }
+
+        public IActionResult resultado (int Calificacion1, int califiacion2, int calificacion3)
+        {
+            return Ok((Calificacion1 + califiacion2 + calificacion3) / 3);
+        }
+
+    
+    }
+}
---
+++ Actividad2U1/Models/PaginaPromedioViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Actividad2U1.Models
+{
+    public class PaginaPromedioViewModel
+    {
+        public int Calificacion1 { get; set; }
+        public int Calificacion2 { get; set; }
+        public int Calificacion3 { get; set; }
+
+        public int resultado { get { return (Calificacion1 + Calificacion2 + Calificacion3)/3; } }
+    }
+}
