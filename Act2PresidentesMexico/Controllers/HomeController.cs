a6e34e0e22d18749f135e98afc8b952b812ee07a
171G0213@rcarbonifera.tecnm.mx <171G0213@rcarbonifera.tecnm.mx>
2021-09-27 18:58:46 -0500

    Clase 1 A2U1

---
+++ Act2PresidentesMexico/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Act2PresidentesMexico.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Act2PresidentesMexico.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            presidentesContext context = new presidentesContext();
+            IEnumerable<Presidente> presidentes = context.Presidentes.OrderByDescending(x => x.InicioGobierno);
+            return View(presidentes);
+        }
+
+        public IActionResult Biografia ()
+        {
+            return View();
+        }
+    }
+}
