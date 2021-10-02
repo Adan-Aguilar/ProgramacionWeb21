32671d5f19821b6a720fec311b25c64db9228480
171G0213@rcarbonifera.tecnm.mx <171G0213@rcarbonifera.tecnm.mx>
2021-10-01 19:02:52 -0500

    VISTA INFORMACION TERMINADA

--- Actividad3MapaCurricular/Controllers/HomeController.cs
+++ Actividad3MapaCurricular/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Actividad3MapaCurricular.Models;
 
 namespace Actividad3MapaCurricular.Controllers
 {
@@ -10,7 +11,25 @@ namespace Actividad3MapaCurricular.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            mapa_curricularContext context = new mapa_curricularContext(); //Sirve para hacer la conexión a la base de datos
+            var carrera = context.Carreras.OrderBy(x => x.Nombre); //Sirve para traer las carreras de la BD
+
+
+
+            return View(carrera);
+        }
+
+        public IActionResult Informacion (string id)
+        {
+            id=id.Replace("-", " ");
+            mapa_curricularContext context = new mapa_curricularContext();
+            var carrera = context.Carreras.FirstOrDefault(x => x.Nombre == id);
+
+            if (carrera== null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(carrera);
         }
     }
 }
