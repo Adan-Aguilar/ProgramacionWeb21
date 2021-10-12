a235e194770a86b198c9c57f3efae189956bb329
171G0213@rcarbonifera.tecnm.mx <171G0213@rcarbonifera.tecnm.mx>
2021-10-11 20:30:27 -0500

    DisenyPixar

--- Act4DisneyPixar/Controllers/HomeController.cs
+++ Act4DisneyPixar/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Act4DisneyPixar.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Act4DisneyPixar.Controllers
 {
@@ -14,6 +15,7 @@ namespace Act4DisneyPixar.Controllers
             return View();
         }
 
+        [Route("peliculas")]
         public IActionResult Peliculas ()
         {
             pixarContext context = new pixarContext();
@@ -21,8 +23,29 @@ namespace Act4DisneyPixar.Controllers
             return View(peliculas);
         }
 
+        [Route("pelicula/{nombre}")]
+        public IActionResult InformacionPelicula (string nombre)
+        {
+         
+            
+               string nombre2 = nombre== null?"":nombre.Replace("-", " ");
+            
+            
+            pixarContext context = new pixarContext();
+            var pelicula = context.Peliculas
+                .Include(x=>x.Apariciones)
+                .ThenInclude(x=>x.IdPersonajeNavigation)
+                .FirstOrDefault(x => x.Nombre== nombre2||x.Nombre == nombre);
+            return View(pelicula);
+
+            if (pelicula==null)
+            {
+                return RedirectToAction("Index");
+            }
+        }
         public IActionResult Cortos()
         {
+            
             return View();
         }
     }
