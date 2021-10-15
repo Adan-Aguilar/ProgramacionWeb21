414ba3601670e7a8f91feadbddec42265a271714
171G0213@rcarbonifera.tecnm.mx <171G0213@rcarbonifera.tecnm.mx>
2021-10-15 16:00:00 -0500

    Act5U2

---
+++ Act5SweetBakary/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Act5SweetBakary.Models;
+
+namespace Act5SweetBakary.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            sweetbakeryContext context = new sweetbakeryContext();
+            return View();
+        }
+    }
+}
