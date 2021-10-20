959812677eda5848a7c8036ac75ea5ecebce83b7
171G0213@rcarbonifera.tecnm.mx <171G0213@rcarbonifera.tecnm.mx>
2021-10-19 19:54:33 -0500

    ViewModel

--- Act5SweetBakary/Controllers/HomeController.cs
+++ Act5SweetBakary/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Act5SweetBakary.Models;
 using Microsoft.EntityFrameworkCore;
+using Act5SweetBakary.Models.ViewModels;
 
 namespace Act5SweetBakary.Controllers
 {
@@ -31,19 +32,41 @@ namespace Act5SweetBakary.Controllers
         public IActionResult Recetas()
         {
             sweetbakeryContext context = new sweetbakeryContext();
-            var recetas = context.Categorias.Include(x => x.Receta).OrderBy(x => x.Nombre);
+            var recetas = context.Categorias.Include(x => x.Receta).
+                Where(x=>x.Receta.Any())
+               .OrderBy(x => x.Nombre);
             return View(recetas);
         }
 
+        [Route("Receta/{Nombre}")]
+        public IActionResult Recetas(string nombre)
+        {
+            nombre = nombre.Replace("-", " ");
+            sweetbakeryContext context = new sweetbakeryContext();
+            var r = context.Recetas.FirstOrDefault(x => x.Nombre == nombre);
+            if (r==null)
+            {
+                return RedirectToAction("Recetas");
+            }
+
+            else {
+                RecetaViewModel vm = new RecetaViewModel(); 
+                vm.Receta = r;
+                Random rnd = new Random();
+                var datos = context.Recetas
+                    .Where(x=>x.Id!=r.Id)
+                    .OrderBy(x => rnd.Next())
+                    .Take(3)
+                    .Select(x=> new Receta {Id=x.Id, Nombre=x.Nombre });
+                
+                return View(vm);
+
+            }
+
+           
+
+        }
 
 
-        //nombre = nombre.Replace("-", " ");
-        //    sweetbakeryContext context = new sweetbakeryContext();
-        //var r = context.Recetas.FirstOrDefault(x => x.Nombre == nombre);
-        //    if (true)
-        //    {
-
-        //    }
-        //    return View();
     }
 }
---
+++ Act5SweetBakary/Models/ViewModels/RecetaViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Act5SweetBakary.Models.ViewModels
+{
+    public class RecetaViewModel
+    {
+        public Receta Receta { get; set; }
+    }
+}
