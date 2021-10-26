ab64d3a4b7a73c6ed585892d1cf811f3af248a00
171G0213@rcarbonifera.tecnm.mx <171G0213@rcarbonifera.tecnm.mx>
2021-10-25 21:29:24 -0500

    VILLANCICOS

---
+++ ProyectoVillancicos/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoVillancicos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoVillancicos.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            villancicosContext context = new villancicosContext();
+            var villancico = context.Villancicos.OrderBy(x => x.Nombre);
+            return View(villancico);
+        }
+
+        //public IActionResult Villancico()
+        //{
+
+        //}
+    }
+    
+}
